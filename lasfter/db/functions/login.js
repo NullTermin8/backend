4bf1cefe04eaa14b892afc504c6add90fa3e9315
Kasra Jamshidi <lasfter@gmail.com>
2018-01-13 20:09:26 -0800

    Implement login

---
+++ lasfter/db/functions/login.js
@@ -0,0 +1,29 @@
+const { Pool } = require('pg');
+const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
+
+/**
+* Consume email and password and output JWT if password is valid
+* @param {string} email
+* @param {string} password 
+* @returns {string}
+*/
+module.exports = (email, password, context, callback) => {
+  const pool = new Pool({
+    user: process.env.pg_user,
+    host: process.env.pg_host,
+    database: process.env.pg_database,
+    password: process.env.pg_password,
+    port: process.env.pg_port,
+    ssl: true
+  });
+  
+  return pool.query('SELECT * FROM users WHERE email = $1', [email])
+    .then(res => {
+      const hash = res.rows[0].password.trim();
+      const user_info = {email: res.rows[0].email}; 
+      return argon2.verify(hash, password)
+        .then(matched => callback(null, jwt.sign(user_info, process.env.secret)))
+        .catch(err => callback(err));
+    }).catch(err => callback(err));
+};
